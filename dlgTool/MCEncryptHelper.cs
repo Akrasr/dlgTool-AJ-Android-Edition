27df16d09a28debaeafcb0e18cfe47b810ab2510
Akrasr <105559702+Akrasr@users.noreply.github.com>
2022-05-17 21:27:01 +0300

    Initial commit

---
+++ dlgTool/MCEncryptHelper.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace dlgTool
+{
+    static class MCEncryptHelper
+    {
+		public static string ToHashKey(this byte[] data)
+		{
+			byte[] value = null;
+			using (MD5CryptoServiceProvider md5CryptoServiceProvider = new MD5CryptoServiceProvider())
+			{
+				value = md5CryptoServiceProvider.ComputeHash(data);
+			}
+			return BitConverter.ToString(value).ToLower().Replace("-", string.Empty);
+		}
+
+		// Token: 0x06000C1C RID: 3100 RVA: 0x0041465F File Offset: 0x0041285F
+		public static string ToHashKey(this string str)
+		{
+			return Encoding.Unicode.GetBytes(str).ToHashKey();
+		}
+
+		// Token: 0x06000C1D RID: 3101 RVA: 0x004640C4 File Offset: 0x004622C4
+		public static void WriteToFile(byte[] data, string key, string filePath)
+		{
+			using (TripleDESCryptoServiceProvider tripleDESCryptoServiceProvider = new TripleDESCryptoServiceProvider())
+			{
+				tripleDESCryptoServiceProvider.Key = MCEncryptHelper.GenerateKey(key, tripleDESCryptoServiceProvider.Key.Length);
+				tripleDESCryptoServiceProvider.IV = MCEncryptHelper.GenerateKey(key, tripleDESCryptoServiceProvider.IV.Length);
+				using (FileStream fileStream = File.Create(filePath))
+				{
+					using (CryptoStream cryptoStream = new CryptoStream(fileStream, tripleDESCryptoServiceProvider.CreateEncryptor(), CryptoStreamMode.Write))
+					{
+						cryptoStream.Write(data, 0, data.Length);
+					}
+				}
+			}
+		}
+
+		// Token: 0x06000C1E RID: 3102 RVA: 0x00464180 File Offset: 0x00462380
+		public static byte[] EncryptData(string key, byte[] sources)
+		{
+			byte[] result = null;
+			using (TripleDESCryptoServiceProvider tripleDESCryptoServiceProvider = new TripleDESCryptoServiceProvider())
+			{
+				tripleDESCryptoServiceProvider.Key = MCEncryptHelper.GenerateKey(key, tripleDESCryptoServiceProvider.Key.Length);
+				tripleDESCryptoServiceProvider.IV = MCEncryptHelper.GenerateKey(key, tripleDESCryptoServiceProvider.IV.Length);
+				using (MemoryStream memoryStream = new MemoryStream())
+				{
+					using (CryptoStream cryptoStream = new CryptoStream(memoryStream, tripleDESCryptoServiceProvider.CreateEncryptor(), CryptoStreamMode.Write))
+					{
+						cryptoStream.Write(sources, 0, sources.Length);
+					}
+					result = memoryStream.ToArray();
+				}
+			}
+			return result;
+		}
+
+		// Token: 0x06000C1F RID: 3103 RVA: 0x00464244 File Offset: 0x00462444
+		public static byte[] ReadFromFile(string key, string filePath)
+		{
+			if (!File.Exists(filePath))
+			{
+				return null;
+			}
+			byte[] result = null;
+			using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
+			{
+				result = MCEncryptHelper.DecryptData(key, fileStream);
+			}
+			return result;
+		}
+
+		// Token: 0x06000C20 RID: 3104 RVA: 0x00464294 File Offset: 0x00462494
+		public static byte[] DecryptData(string key, byte[] sources)
+		{
+			byte[] result = null;
+			using (MemoryStream memoryStream = new MemoryStream(sources))
+			{
+				result = MCEncryptHelper.DecryptData(key, memoryStream);
+			}
+			return result;
+		}
+
+		// Token: 0x06000C21 RID: 3105 RVA: 0x004642D8 File Offset: 0x004624D8
+		public static byte[] DecryptData(string key, Stream sourceStream)
+		{
+			byte[] result = null;
+			using (TripleDESCryptoServiceProvider tripleDESCryptoServiceProvider = new TripleDESCryptoServiceProvider())
+			{
+				tripleDESCryptoServiceProvider.Key = MCEncryptHelper.GenerateKey(key, tripleDESCryptoServiceProvider.Key.Length);
+				tripleDESCryptoServiceProvider.IV = MCEncryptHelper.GenerateKey(key, tripleDESCryptoServiceProvider.IV.Length);
+				using (CryptoStream cryptoStream = new CryptoStream(sourceStream, tripleDESCryptoServiceProvider.CreateDecryptor(), CryptoStreamMode.Read))
+				{
+					using (BinaryReader binaryReader = new BinaryReader(cryptoStream))
+					{
+						result = binaryReader.ReadBytes((int)sourceStream.Length);
+					}
+				}
+			}
+			return result;
+		}
+
+		// Token: 0x06000C22 RID: 3106 RVA: 0x00464398 File Offset: 0x00462598
+		public static byte[] GenerateKey(string key, int length)
+		{
+			byte[] array = new byte[length];
+			for (int i = 0; i < length; i++)
+			{
+				array[i] = (byte)((int)key[i % key.Length] + i & 255);
+			}
+			return array;
+		}
+	}
+}
---
+++ dlgTool/Program.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using dlgTool.CustomEncoding;
+using dlgTool.IO;
+using dlgTool.Compression;
+
+namespace dlgTool
+{
+    class Program
+    {
+        public const string Key = "NsnRg5uTEyyrftIS";
+
+        class DlgEntry
+        {
+            public int offset;
+            public int size;
+        }
+
+        class Options
+        {
+            public string mode = "";
+            public string path = "";
+            public bool isBinary = false;
+        }
+
+        static Options ParseArgs(string[] args)
+        {
+            if (args.Count() == 0)
+            {
+                Console.WriteLine($"" +
+                        $"Usage: dlgTool.exe [PARAMS]\n\n" +
+                        $"PARAMS:\n" +
+                        $"\t-m <mode> - The mode to execute in. Can be \"extract\" or \"create\"\n" +
+                        $"\t-f <path> - The file or folder path. Needs to be a file for \"extract\" and a folder for \"create\"\n" +
+                        $"\t-h - Shows this help\n" +
+                        $"\t-b - Extracts or recreates by using the binary files instead of txt's");
+                Environment.Exit(0);
+            }
+
+            var opt = new Options();
+            List<string> modes = new List<string> { "-m", "-f", "-b", "-h" };
+
+            for (int i = 0; i < args.Count(); i++)
+            {
+                var param = args[i];
+                if (!modes.Contains(param))
+                {
+                    Console.WriteLine($"{param} is an unknown mode.");
+                    Environment.Exit(0);
+                }
+                if (param != "-b" && param != "-h")
+                {
+                    if (i + 1 < args.Count())
+                    {
+                        if (args[i + 1][0] == '-')
+                        {
+                            Console.WriteLine($"No value is given for {param}");
+                            Environment.Exit(0);
+                        }
+                        else
+                        {
+                            var value = args[i++ + 1];
+                            switch (param)
+                            {
+                                case "-m":
+                                    if (value != "extract" && value != "create")
+                                    {
+                                        Console.WriteLine($"Mode can only be \"extract\" or \"create\".");
+                                        Environment.Exit(0);
+                                    }
+                                    else
+                                    {
+                                        opt.mode = value;
+                                    }
+                                    break;
+                                case "-f":
+                                    opt.path = value;
+                                    break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"No value is given for {param}");
+                        Environment.Exit(0);
+                    }
+                }
+                else if (param == "-h")
+                {
+                    Console.WriteLine($"" +
+                        $"Usage: dlgTool.exe [PARAMS]\n\n" +
+                        $"PARAMS:\n" +
+                        $"\t-m <mode> - The mode to execute in. Can be \"extract\" or \"create\"\n" +
+                        $"\t-f <path> - The file or folder path. Needs to be a file for \"extract\" and a folder for \"create\"" +
+                        $"\t-h - Shows this help\n" +
+                        $"\t-b - Extracts or recreates by using the binary files instead of txt's");
+                    Environment.Exit(0);
+                }
+                else if (param == "-b")
+                {
+                    opt.isBinary = true;
+                }
+
+            }
+
+            //Check set options
+            if (opt.mode == String.Empty)
+            {
+                Console.WriteLine($"No Mode is given.");
+                Environment.Exit(0);
+            }
+            if (opt.path == String.Empty)
+            {
+                Console.WriteLine($"No Path is given.");
+                Environment.Exit(0);
+            }
+            if (opt.mode == "create")
+            {
+                if ((File.GetAttributes(opt.path) & FileAttributes.Directory) != FileAttributes.Directory)
+                {
+                    Console.WriteLine($"Path has to be a folder.");
+                    Environment.Exit(0);
+                }
+                if (!Directory.Exists(opt.path))
+                {
+                    Console.WriteLine($"Folder doesn't exist.");
+                    Environment.Exit(0);
+                }
+            }
+            if (opt.mode == "extract")
+            {
+                if ((File.GetAttributes(opt.path) & FileAttributes.Directory) == FileAttributes.Directory)
+                {
+                    Console.WriteLine($"Path has to be a file.");
+                    Environment.Exit(0);
+                }
+                if (!File.Exists(opt.path))
+                {
+                    Console.WriteLine($"File doesn't exist.");
+                    Environment.Exit(0);
+                }
+            }
+
+            return opt;
+        }
+
+        static void Main(string[] args2)
+        {
+            var options = ParseArgs(args2);
+
+            if (options.mode == "extract")
+            {
+                var sections = new List<byte[]>();
+                using (var br = new BinaryReaderY(File.OpenRead(options.path)))
+                {
+                    try
+                    {
+                        var entryCount = br.ReadInt32();
+                        var entryList = br.ReadMultiple<DlgEntry>(entryCount);
+                        foreach (var e in entryList)
+                        {
+                            Console.WriteLine("" + e.offset + " " + e.size);
+                            br.BaseStream.Position = e.offset;
+                            sections.Add(Nintendo.Decompress(new MemoryStream(MCEncryptHelper.DecryptData(Key, br.ReadBytes(e.size)))));
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("This file seems a bit corrupted or isn't a dlg. Either way it won't work.");
+                        Console.WriteLine(ex.Message + " - " + ex.InnerException);
+                        return;
+                    }
+
+                }
+                var writeDir = Path.Combine(Path.GetDirectoryName(options.path), Path.GetFileNameWithoutExtension(options.path));
+                if (!Directory.Exists(writeDir))
+                    Directory.CreateDirectory(writeDir);
+
+                var txtCount = 0;
+                foreach (var s in sections)
+                {
+                    using (var br = new BinaryReaderY(new MemoryStream(s)))
+                    {
+                        if (options.isBinary)
+                        {
+                            File.WriteAllBytes(Path.Combine(writeDir, $"{txtCount++:000}.bin"), s);
+                        }
+                        else
+                        {
+                            var enc = new AAEncoding();
+                            File.WriteAllText(Path.Combine(writeDir, $"{txtCount++:000}.txt"), enc.GetSectionText(s));
+                        }
+                    }
+                }
+            }
+            else if (options.mode == "create")
+            {
+                var enc = new AAEncoding();
+                var files = Directory.GetFiles(options.path).Where(f => Path.GetExtension(f) == ((options.isBinary) ? ".bin" : ".txt")).OrderBy(name => name).ToList();
+                var entries = new List<DlgEntry>();
+
+                var ms = new MemoryStream();
+                using (var bw = new BinaryWriterY(ms, true))
+                {
+                    bw.BaseStream.Position = files.Count * 8 + 4;
+
+                    foreach (var file in files)
+                    {
+                        byte[] compBytes = null;
+                        if (options.isBinary)
+                        {
+                            compBytes = File.ReadAllBytes(file);
+                        }
+                        else
+                        {
+                            var sectionText = File.ReadAllText(file).Replace("\r\n", "");
+                            var bytes = enc.GetBytes(sectionText);
+                            compBytes = bytes;
+                        }
+                        compBytes = MCEncryptHelper.EncryptData(Key, compBytes);
+
+                        entries.Add(new DlgEntry { offset = (int)bw.BaseStream.Position, size = compBytes.Length });
+
+                        bw.Write(compBytes);
+                        bw.BaseStream.Position = (bw.BaseStream.Position);
+                    }
+
+                    bw.BaseStream.Position = 0;
+                    bw.Write(entries.Count);
+                    foreach (var entry in entries)
+                        bw.WriteStruct(entry);
+                }
+
+                File.WriteAllBytes("mes_all_edited.txt", ms.ToArray());
+            }
+        }
+    }
+}
